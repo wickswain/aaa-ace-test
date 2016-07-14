8ce1e3598ce20bfc8415217cfa9de67f7c9578d6
Vagnar Polund <vpolund@axis41.com>
2016-07-14 10:03:50 -0600

    make example titlesightly component

---
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/TitleSightlyProvider.java
@@ -0,0 +1,23 @@
+package com.aaa.ace.sightly.providers;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+import org.apache.sling.api.resource.ValueMap;
+
+public class TitleSightlyProvider extends WCMUsePojo {
+
+	private String jcrtitle;
+
+	@Override
+	public void activate() throws Exception {
+
+		ValueMap properties = getProperties();
+
+		jcrtitle=properties.get("jcr:title",String.class);
+
+	}
+
+	public String getJcrtitle(){
+		return jcrtitle;
+	}
+
+}
