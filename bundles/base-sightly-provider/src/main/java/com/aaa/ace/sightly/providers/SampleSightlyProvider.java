2bceb4dd25f24fa59d5efc1b4e42fdc5fffd623a
Bharath Kambam <bkambam@axis41.com>
2016-07-20 18:29:31 +0530

    [AAA-48]: Added html-prototype project as maven module to the project structure.

--- bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/SampleSightlyProvider.java
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/SampleSightlyProvider.java
@@ -2,12 +2,17 @@ package com.aaa.ace.sightly.providers;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
+/**
+ * This is a sample sightly provider class and can be removed at later stage.
+ * @author bharath.kambam
+ *
+ */
 public class SampleSightlyProvider extends WCMUsePojo {
 
-	@Override
-	public void activate() throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void activate() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
 
 }
