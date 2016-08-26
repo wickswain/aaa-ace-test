56da56d4cab709552b71b8acd2269d3ba7daacda
Manish Kumar Singh <msingh@axis41.com>
2016-08-26 22:54:18 +0530

    [AAA-83]:Minor bugs fixed. 1. Sightly Provider added in order to append page suffix as "html" in case internal AEM path is authored as link URLs. 2. Inline background color style code updated.

---
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/FAQProvider.java
@@ -0,0 +1,41 @@
+package com.aaa.ace.sightly.providers;
+
+import com.aaa.ace.services.PageSuffixResolverService;
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class FAQProvider extends WCMUsePojo{
+	
+	private String url1;
+	private String url2;
+
+	@Override
+	public void activate() throws Exception {
+		url1 = get("url1", String.class);
+		url2 = get("url2", String.class);
+		url1 = addPageSuffix(url1);
+		url2 = addPageSuffix(url2);
+	}
+	
+	private String addPageSuffix(String url){
+		if (url != null && url.startsWith("/content")) {
+			PageSuffixResolverService pageService = getSlingScriptHelper().getService(PageSuffixResolverService.class);
+			return pageService.resolveLinkURL(getResourceResolver(), url);
+		}
+		return null;
+	}
+	
+	/**
+	 * @return the url1
+	 */
+	public String getUrl1() {
+		return url1;
+	}
+	
+	/**
+	 * @return the url2
+	 */
+	public String getUrl2() {
+		return url2;
+	}
+
+}
