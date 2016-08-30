4ac051f4c4ff6f1502df9b075488f6b85a96160b
Manish Kumar Singh <msingh@axis41.com>
2016-08-30 16:24:37 +0530

    [AAA-83]: changes against review comments incorporated. 1. Sightly Provider code modified, getProperties() method is being used now to fetch authored properties from component dialog.

--- bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/PageSuffixProvider.java
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/PageSuffixProvider.java
@@ -1,23 +1,30 @@
 package com.aaa.ace.sightly.providers;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aaa.ace.services.PageSuffixResolverService;
 import com.adobe.cq.sightly.WCMUsePojo;
 
-public class FAQProvider extends WCMUsePojo{
+public class PageSuffixProvider extends WCMUsePojo{
 	
+	private static final Logger log = LoggerFactory.getLogger(PageSuffixProvider.class);
 	private String url1;
 	private String url2;
 
 	@Override
 	public void activate() throws Exception {
-		url1 = get("url1", String.class);
-		url2 = get("url2", String.class);
+		url1 = getProperties().get("link1URL", String.class);
+		log.debug("url1 inside PageSuffixProvider class: " + url1);
+		url2 = getProperties().get("link2URL", String.class);
+		log.debug("url2 inside PageSuffixProvider class: " + url2);
 		url1 = addPageSuffix(url1);
 		url2 = addPageSuffix(url2);
 	}
 	
 	private String addPageSuffix(String url){
-		if (url != null && url.startsWith("/content")) {
+		if (StringUtils.isNotBlank(url) && url.startsWith("/content")) {
 			PageSuffixResolverService pageService = getSlingScriptHelper().getService(PageSuffixResolverService.class);
 			return pageService.resolveLinkURL(getResourceResolver(), url);
 		}
