655dec530f2807d8acebd7730bc5837960d11198
SLS\cssavirigana <csavirigana@axis41.com>
2016-09-09 21:31:11 +0530

    [AAA-FED]: Sprint 3 Front-End UI [AAA-90] Component - Home Header (Front-End)

--- html-prototype/clientlibs/ace-www/base/js/scripts.js
+++ html-prototype/clientlibs/ace-www/base/js/scripts.js
@@ -1,13 +1,14 @@
 /*****************************************
     Template : AAA Scripts
     Created Date:12-August-2016
-    Modified Date:08-Sep-2016
-    Version:0.4
+    Modified Date:09-September-2016
+    Version:0.5
 *****************************************/
 $(function () {
-
+    /*Fixed Header - alignment*/
+    $('#page-container').css('paddingTop', $('#page-header').outerHeight());
     /*Footer Responsive script*/
-    var footerResponse = function () {
+    function footerResponse() {
         if ($(window).width() <= 992) {
             $('.list-header').each(function () {
                 $(this).next().removeClass('in');
@@ -15,28 +16,26 @@ $(function () {
         } else {
             $('.list-header').parent().find('ul').addClass('in').removeAttr('style');
         }
-    };
+    }
     footerResponse();
-
     /*Sticky Navigation*/
     var scrollTop = ($('.sticky-nav').hasClass('sticky-nav')) ? parseInt($('.sticky-nav').offset().top) : 0;
-    var stickynav = function () {
+
+    function stickynav() {
         //$('.sticky-nav').height();
         $('.sticky-navbar').width($('.container-custom').outerWidth());
-    };
+    }
     stickynav();
     $('.sticky-nav .dropdown-menu li > a').click(function (e) {
         var hashtag = $(this.hash),
-            $target = parseInt(hashtag.offset().top) - $('.sticky-navbar').outerHeight()
+            $target = parseInt(hashtag.offset().top) - $('.sticky-navbar').outerHeight();
         $('html,body').stop().animate({
             scrollTop: $target
         }, 1000, 'swing');
         e.preventDefault();
     });
-
     /*Page Scroll*/
     $(window).scroll(function () {
-
         /*Sticky Nav scroll event*/
         ($(this).scrollTop() > scrollTop) ? $('.sticky-navbar').addClass('navbar-fixed-top'): $('.sticky-navbar').removeClass('navbar-fixed-top');
         $('.sticky-nav .dropdown-menu li > a').each(function () {
@@ -48,8 +47,17 @@ $(function () {
             }
         });
     });
-
-
+    /*Home Header resize*/
+    function headerResize() {
+        $('.home-header').each(function () {
+            if ($(this).height() < $(window).height() / 2 - $('#page-header').outerHeight() && $(this).hasClass('small')) $(this).height($(window).height() / 2 - $('#page-header').outerHeight());
+            if ($(this).height() < $(window).height() / 4 * 3 - $('#page-header').outerHeight() && $(this).hasClass('medium')) $(this).height($(window).height() / 4 * 3 - $('#page-header').outerHeight());
+            if ($(this).height() < $(window).height() - $('#page-header').outerHeight() && $(this).hasClass('full')) $(this).height($(window).height() - $('#page-header').outerHeight());
+        });
+    }
+    headerResize();
+    /*Home Header - jump link*/
+    $('.learn-link a').attr('href', '#' + $('.home-header').next().attr('id'));
     $('.home-header .learn-link a').click(function (e) {
         var hashtag = $(this.hash),
             $target = parseInt(hashtag.offset().top);
@@ -58,9 +66,9 @@ $(function () {
         }, 1000, 'swing');
         e.preventDefault();
     });
-
     /*Responsive*/
     $(window).resize(function () {
+        headerResize();
         footerResponse();
         stickynav();
     });
