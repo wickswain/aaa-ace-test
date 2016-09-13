0f6b08703349726ea3ebe8603b757ca01169a5b5
yogesh.mahajan <yogesh.mahajan@axis41.com>
2016-09-13 18:32:24 +0530

    [AAA-105]: Article Carousel Initial commit.
    - Added component, html
    - Modified ChildResourceProvider to return length of child nodes

--- bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/ChildResourceProvider.java
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/ChildResourceProvider.java
@@ -1,11 +1,15 @@
 package com.aaa.ace.sightly.providers;
 
+import java.util.Iterator;
+
 import org.apache.sling.api.resource.Resource;
+
 import com.adobe.cq.sightly.WCMUsePojo;
 
+
+
 /**
- * This sightly provider is used to get the child resource properties of current
- * resource.
+ * This sightly provider is used to get the child resource properties of current resource.
  *
  */
 public class ChildResourceProvider extends WCMUsePojo {
@@ -24,10 +28,29 @@ public class ChildResourceProvider extends WCMUsePojo {
     /**
      * gets the child resource matching with the name passed as parameter.
      *
-     * @return
+     * @return Resource
      */
     public Resource getChildResource() {
         return childResource;
+    }
+
+    /**
+     * Get number of children.
+     */
+    public Integer getChildLength() {
+        int length = 0;
+
+        // Could not find any API that returns number of children so have to loop through and count.
+        Iterator<Resource> resourceIterator = childResource.listChildren();
+
+        while (resourceIterator.hasNext()) {
+            resourceIterator.next();
+            length++;
+        }
+        
+        
+        return length;
+
     }
 
 }
