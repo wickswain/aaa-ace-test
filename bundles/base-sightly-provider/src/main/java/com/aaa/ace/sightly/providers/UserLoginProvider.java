d7cfa6e783bf3a13f94befe19bf9b692443c10a9
Bharath Kambam <bkambam@axis41.com>
2016-09-23 16:11:28 +0530

    [AAA-157]: Login cookie changes and global header latest mock up html integration.

--- bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/UserLoginProvider.java
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/UserLoginProvider.java
@@ -3,6 +3,9 @@ package com.aaa.ace.sightly.providers;
 import javax.servlet.http.Cookie;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
@@ -14,20 +17,79 @@ import com.adobe.cq.sightly.WCMUsePojo;
  */
 public class UserLoginProvider extends WCMUsePojo {
 
+    private static final Logger log = LoggerFactory.getLogger(UserLoginProvider.class);
+
+    private static final String STRING_EQUAL_SYMBOL = "=";
+    private static final String STRING_AND_SYMBOL = "&";
+    private static final String PROPERTY_LAST_NAME = "LastName";
+    private static final String PROPERTY_FIRST_NAME = "FirstName";
+    private static final String COOKIE_ACEUSER = "aceuser";
+    private static final String COOKIE_ASPXAUTH = ".ASPXAUTH";
+
     boolean isLoggedIn = false;
     String firstName;
     String lastName;
+    String signInURL;
+    String signOutURL;
 
     @Override
     public void activate() throws Exception {
-        Cookie logInCookie = getRequest().getCookie("aceu");
+        String requestURL = getRequestURL(getRequest());
+        log.debug("Request URL:" + requestURL);
+
+        signInURL = constructURL(get("signInURL", String.class), requestURL,
+                getCurrentPage().getPath());
+        log.debug("SignIN URL:" + signInURL);
+
+        signOutURL = constructURL(get("signOutURL", String.class), requestURL,
+                getCurrentPage().getPath());
+        log.debug("SignOut URL:" + signOutURL);
+
+        Cookie logInCookie = getRequest().getCookie(COOKIE_ASPXAUTH);
 
         if (logInCookie != null) {
             isLoggedIn = true;
-            firstName = fetchCookieValue("aceuser", "FirstName");
-            lastName = fetchCookieValue("aceuser", "LastName");
+            firstName = fetchCookieValue(COOKIE_ACEUSER, PROPERTY_FIRST_NAME);
+            lastName = fetchCookieValue(COOKIE_ACEUSER, PROPERTY_LAST_NAME);
 
         }
+    }
+
+    /**
+     * Gets the request URL by appending the request scheme, host and port.
+     *
+     * @param request
+     * @return
+     */
+    private String getRequestURL(SlingHttpServletRequest request) {
+        String scheme = request.getScheme();
+        String serverName = request.getServerName();
+        int serverPort = request.getServerPort();
+
+        // Reconstruct original requesting URL
+        StringBuilder url = new StringBuilder();
+        url.append(scheme).append("://").append(serverName).append(":").append(serverPort);
+
+        return url.toString();
+    }
+
+    /**
+     * Construct the Final URL.
+     *
+     * @param url
+     * @param requestURL
+     * @param currentPagePath
+     * @return
+     */
+    private String constructURL(String url, String requestURL, String currentPagePath) {
+        String finalURL = "";
+        log.debug("URL from CTA Provider: " + url);
+
+        if (StringUtils.isNotBlank(url)) {
+            finalURL = url + "?ReturnURL=" + requestURL + currentPagePath;
+            finalURL = (currentPagePath.endsWith(".html") ? finalURL : (finalURL + ".html"));
+        }
+        return finalURL;
     }
 
     /**
@@ -38,11 +100,11 @@ public class UserLoginProvider extends WCMUsePojo {
         Cookie aceuser = getRequest().getCookie(cookieName);
 
         if (aceuser != null && StringUtils.isNotBlank(aceuser.getValue())) {
-            String[] cookieItems = aceuser.getValue().split("&");
+            String[] cookieItems = aceuser.getValue().split(STRING_AND_SYMBOL);
 
             for (String cookieItem : cookieItems) {
                 if (cookieItem.contains(key)) {
-                    cookieValue = cookieItem.substring(cookieItem.indexOf("=") + 1);
+                    cookieValue = cookieItem.substring(cookieItem.indexOf(STRING_EQUAL_SYMBOL) + 1);
                 }
             }
         }
@@ -69,6 +131,20 @@ public class UserLoginProvider extends WCMUsePojo {
      */
     public String getLastName() {
         return lastName;
+    }
+
+    /**
+     * @return the signInURL
+     */
+    public String getSignInURL() {
+        return signInURL;
+    }
+
+    /**
+     * @return the signOutURL
+     */
+    public String getSignOutURL() {
+        return signOutURL;
     }
 
 }
