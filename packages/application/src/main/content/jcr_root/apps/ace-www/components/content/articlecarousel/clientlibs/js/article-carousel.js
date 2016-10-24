d9a8219c7ec2327ef4cc85171af2ca724a9c63fe
Bharath Kambam <bkambam@axis41.com>
2016-10-24 13:08:37 +0530

    [AAA-323]: Article carousel defect fix.

--- packages/application/src/main/content/jcr_root/apps/ace-www/components/content/articlecarousel/clientlibs/js/article-carousel.js
+++ packages/application/src/main/content/jcr_root/apps/ace-www/components/content/articlecarousel/clientlibs/js/article-carousel.js
@@ -1,42 +1,50 @@
-$j(function($){
+$('#article-carousel').find('.carousel-control.left').css({
+	'cursor' : 'wait',
+	'pointer-events' : 'none',
+	'opacity' : '0.1'
+});
 
-	$('#article-carousel').find('.carousel-control.left')
-						  .css({'cursor': 'wait',
-						        'pointer-events': 'none',
-								'opacity': '0.1'});
-	
-	$('.carousel-text .image-label').text($('div.active').data('imagelabel') + ' | ' + $('div.active').data('author'));
-	$('.right-align').text($('div.active').index() + 1 + ' of ' + $('.carousel-inner .item').length);
-								
-	$('#article-carousel').bind('slid.bs.carousel', function (e) {
-    
-        var $this = $(this);
+$('.carousel-text .image-label').text(
+		$('div.active').data('imagelabel') + ' | '
+				+ $('div.active').data('author'));
+$('.right-align').text(
+		$('div.active').index() + 1 + ' of '
+				+ $('.carousel-inner .item').length);
 
-        $this.children('.carousel-control').show();
-        $('#article-carousel').carousel('pause');
-		
-		$('.carousel-text .image-label').text($('div.active').data('imagelabel') + ' | ' + $('div.active').data('author'));
-		$('.right-align').text($('div.active').index() + 1 + ' of ' + $('.carousel-inner .item').length);
-	
-		
-        if ($('.carousel-inner .item:first').hasClass('active')) {
-            $this.children('.carousel-control.left')
-				 .css({'cursor': 'wait',
-				       'pointer-events': 'none',
-					   'opacity': '0.1'});
-        } else {
-            $this.children('.carousel-control.left').removeAttr('style');
-        }
-        
-        if ($('.carousel-inner .item:last').hasClass('active')) {
-			$this.children('.carousel-control.right')
-				 .css({ 'cursor': 'wait',
-						'pointer-events': 'none',
-						'opacity': '0.1'});
-        } else {
-            $this.children('.carousel-control.right').removeAttr('style');
-        }
-		
-    });
+$('#article-carousel').bind(
+		'slid.bs.carousel',
+		function(e) {
+
+	var $this = $(this);
+
+	$this.children('.carousel-control').show();
+	$('#article-carousel').carousel('pause');
+
+	$('.carousel-text .image-label').text(
+			$('div.active').data('imagelabel') + ' | '
+					+ $('div.active').data('author'));
+	$('.right-align').text(
+			$('div.active').index() + 1 + ' of '
+					+ $('.carousel-inner .item').length);
+
+	if ($('.carousel-inner .item:first').hasClass('active')) {
+		$this.children('.carousel-control.left').css({
+			'cursor' : 'wait',
+			'pointer-events' : 'none',
+			'opacity' : '0.1'
+		});
+	} else {
+		$this.children('.carousel-control.left').removeAttr('style');
+	}
+
+	if ($('.carousel-inner .item:last').hasClass('active')) {
+		$this.children('.carousel-control.right').css({
+			'cursor' : 'wait',
+			'pointer-events' : 'none',
+			'opacity' : '0.1'
+		});
+	} else {
+		$this.children('.carousel-control.right').removeAttr('style');
+	}
 
 });
