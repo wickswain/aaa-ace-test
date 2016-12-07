fa2eed87ebb4eb807f61c4e28a4e5c0db968daa0
Bharath Kambam <bkambam@axis41.com>
2016-12-07 13:01:44 +0530

    [AAA]: Added the latest changes for card-carousel and travel-widget components from Raizlabs to AAA Core.

--- packages/application/src/main/content/jcr_root/apps/aaa-core/components/content/card-carousel/clientlibs/js/card-carousel.js
+++ packages/application/src/main/content/jcr_root/apps/aaa-core/components/content/card-carousel/clientlibs/js/card-carousel.js
@@ -1,23 +1,19 @@
-var main = function() {
-    (function(){
-        $('.carousel-showmanymoveone .item').each(function(){
-            var itemToClone = $(this);
+$j(function ($) {
+    $('.carousel-showmanymoveone .item').each(function(){
+        var itemToClone = $(this);
+        
+        for (var i=1;i<4;i++) {
+            itemToClone = itemToClone.next();
             
-            for (var i=1;i<4;i++) {
-                itemToClone = itemToClone.next();
-                
-                // wrap around if at end of item collection
-                if (!itemToClone.length) {
-                    itemToClone = $(this).siblings(':first');
-                }
-                
-                // grab item, clone, add marker class, add to collection
-                itemToClone.children(':first-child').clone()
-                .addClass("cloneditem-"+(i))
-                .appendTo($(this));
+            // wrap around if at end of item collection
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
             }
-        });
-    }());
-}
-
-$(document).ready(main);
+            
+            // grab item, clone, add marker class, add to collection
+            itemToClone.children(':first-child').clone()
+            .addClass("cloneditem-"+(i))
+            .appendTo($(this));
+        }
+    });
+});
