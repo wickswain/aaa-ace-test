f93d2aa2b9cc52cd41b2c96b8c7339b7bab65d59
hardikdesai <hdesai@axis41.com>
2017-01-13 17:02:55 +0530

    [AAA-424] I have added a js file to restrict images on RTE component.

---
+++ packages/application/src/main/content/jcr_root/etc/clientlibs/aaa-core/editmode/touchui-editor/js/rte-image-restriction.js
@@ -0,0 +1,27 @@
+(function (document, $, ns) {
+"use strict"; 
+$(document).on("click", ".cq-dialog-submit", function (e) {
+	e.stopPropagation();
+	e.preventDefault();
+	var $form = $(this).closest("form.foundation-form"),
+	RTEimg = $form.find("[name='./text']").val(),
+	message, clazz = "coral-Button ";
+	if(RTEimg.includes('<img src="/content/dam/')){
+	ns.ui.helpers.prompt({
+	title: Granite.I18n.get("Invalid Input"),
+	message: "Images are not allowed here",
+	actions: [{
+	id: "CANCEL",
+	text: "CANCEL",
+	className: "coral-Button"
+	}],
+	callback: function (actionId) {
+	if (actionId === "CANCEL") {
+	}
+	}
+	});
+	}else{
+	$form.submit();
+	}
+	});
+})(document, Granite.$, Granite.author);
