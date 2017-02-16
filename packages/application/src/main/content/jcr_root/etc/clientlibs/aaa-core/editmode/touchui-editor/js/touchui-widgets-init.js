6bbd2e06cdf17009f737a23ee7ea2b1ae6515f30
hardikdesai <hdesai@axis41.com>
2017-02-16 13:25:09 +0530

    [AAA-457] : Updated Card Carousel component.

--- packages/application/src/main/content/jcr_root/etc/clientlibs/aaa-core/editmode/touchui-editor/js/touchui-widgets-init.js
+++ packages/application/src/main/content/jcr_root/etc/clientlibs/aaa-core/editmode/touchui-editor/js/touchui-widgets-init.js
@@ -20,7 +20,13 @@
  * A sample component dialog using the Touch UI Multi Field
  * Note the usage of empty valued acs-commons-nested property
  */
-(function () {
+
+/*
+ * Line No.142 - 170: Added the logic to validate the minimum number of items on multifield. In this case, we need throw an error and disable the dialog form submit.
+ * Line No.200 - 217: Added the logic to validate the minimum and maximum number of items on multifield.
+*/
+
+(function() {
     "use strict";
 
     if (typeof window.ACS === "undefined") {
@@ -33,17 +39,17 @@
 
     ACS.TouchUI.Widget = new Class({
         toString: 'ACS TouchUI Widget Base Class',
-        ACS_COMMONS_NESTED:  "acs-commons-nested",
-        DATA_ACS_COMMONS_NESTED:  "data-acs-commons-nested",
-        CFFW:  ".coral-Form-fieldwrapper",
+        ACS_COMMONS_NESTED: "acs-commons-nested",
+        DATA_ACS_COMMONS_NESTED: "data-acs-commons-nested",
+        CFFW: ".coral-Form-fieldwrapper",
         JSON_STORE: "JSON_STORE",
         NODE_STORE: "NODE_STORE",
 
-        isSelectOne: function ($field) {
+        isSelectOne: function($field) {
             return !_.isEmpty($field) && ($field.prop("type") === "select-one");
         },
 
-        setSelectOne: function ($field, value) {
+        setSelectOne: function($field, value) {
             var select = $field.closest(".coral-Select").data("select");
 
             if (select) {
@@ -51,35 +57,35 @@
             }
         },
 
-        isCheckbox: function ($field) {
+        isCheckbox: function($field) {
             return !_.isEmpty($field) && ($field.prop("type") === "checkbox");
         },
 
-        setCheckBox: function ($field, value) {
+        setCheckBox: function($field, value) {
             $field.prop("checked", $field.attr("value") === value);
         },
 
-        isDateField: function ($field) {
+        isDateField: function($field) {
             return !_.isEmpty($field) && $field.prop("type") === "hidden" && $field.parent().hasClass("coral-DatePicker");
         },
 
-        setDateField: function ($field, value) {
+        setDateField: function($field, value) {
             var date = moment(new Date(value));
             var $parent = $field.parent();
             $parent.find("input.coral-Textfield").val(date.format($parent.attr("data-displayed-format")));
             $field.val(date.format($parent.attr("data-stored-format")));
         },
 
-        isRichTextField: function ($field) {
+        isRichTextField: function($field) {
             return !_.isEmpty($field) && $field.prop("type") === "hidden" && !$field.hasClass("coral-RichText-isRichTextFlag") && $field.parent().hasClass("richtext-container");
         },
 
-        setRichTextField: function ($field, value) {
+        setRichTextField: function($field, value) {
             $field.val(value);
             $field.parent().find(".coral-RichText-editable.coral-RichText").empty().append(value);
         },
 
-        setWidgetValue: function ($field, value) {
+        setWidgetValue: function($field, value) {
             if (_.isEmpty($field)) {
                 return;
             }
@@ -97,29 +103,28 @@
             }
         },
 
-        isJsonStore: function(name){
+        isJsonStore: function(name) {
             return (_.isEmpty(name) || name === this.JSON_STORE);
         },
 
-        isNodeStore: function(name){
+        isNodeStore: function(name) {
             return (name === this.NODE_STORE);
         },
 
-        addCompositeMultifieldRemoveListener: function($multifield){
+        addCompositeMultifieldRemoveListener: function($multifield) {
             var cmf = this;
 
-            $multifield.find(".js-coral-Multifield-remove").click(function(){
+            $multifield.find(".js-coral-Multifield-remove").click(function() {
 
-                setTimeout(function () {
+                setTimeout(function() {
                     cmf.addCompositeMultifieldValidator();
                 }, 500);
-			});
+            });
         },
 
-        addCompositeMultifieldValidator: function(){
-            var boolmessage= false,limtmessage=false;
+        addCompositeMultifieldValidator: function() {
             var fieldErrorEl = $("<span class='coral-Form-fielderror coral-Icon coral-Icon--alert coral-Icon--sizeS' " +
-                                "data-init='quicktip' data-quicktip-type='error' />"),
+                    "data-init='quicktip' data-quicktip-type='error' />"),
                 cmf = this,
                 selector = "[" + cmf.DATA_ACS_COMMONS_NESTED + "] >* input, [" + cmf.DATA_ACS_COMMONS_NESTED + "] >* textarea";
 
@@ -130,84 +135,82 @@
                 clear: clear
             });
 
-            function validate($el){
+            function validate($el) {
                 var $multifield = $el.closest(".coral-Multifield"),
                     $inputs = $multifield.find("input, textarea"),
                     $input, isRequired, message = null;
 
 
-                var value, min,max;
+                var value, min, max;
                 var $multifield = $el.closest(".coral-Multifield");
                 var val = $multifield.children('ol').children();
                 value = parseInt(val.length, 10);
                 min = $multifield.data('min');
                 max = $multifield.data('max');
 
-				if (value > max) {
-					  message="The field must contain "+max+" or less items.";
+                if (value > max) {
+                    message = "The field must contain " + max + " or less items.";
                 } else if (value < min) {
                     if (message == null) {
-						message="The field must contain "+min+" or more items.";
+                        message = "The field must contain " + min + " or more items.";
                     }
                     var arrow = $el.closest("form").hasClass("coral-Form--vertical") ? "right" : "top";
 
-                    var existedFieldErrorSpan= $el.closest("form").find("span.coral-Form-fielderror");
+                    var existedFieldErrorSpan = $el.closest("form").find("span.coral-Form-fielderror");
 
-                    if(!_.isEmpty(existedFieldErrorSpan.attr("class")))
-                    {
-						existedFieldErrorSpan.remove();
+                    if (!_.isEmpty(existedFieldErrorSpan.attr("class"))) {
+                        existedFieldErrorSpan.remove();
                     }
 
                     fieldErrorEl.clone()
-                    .attr("data-quicktip-arrow", arrow)
-                    .attr("data-quicktip-content", message)
-                    .insertAfter($multifield);
-                }else{
-					$(".cq-dialog-submit").removeAttr("disabled");
+                        .attr("data-quicktip-arrow", arrow)
+                        .attr("data-quicktip-content", message)
+                        .insertAfter($multifield);
+                } else {
+                    $(".cq-dialog-submit").removeAttr("disabled");
                     $multifield.nextAll(".coral-Form-fielderror").tooltip("hide").remove();
                 }
-                $inputs.each(function(index, input){
+                $inputs.each(function(index, input) {
                     $input = $(input);
                     isRequired = $input.attr("required") || ($input.attr("aria-required") === "true");
 
                     if (isRequired && $input.val().length === 0) {
                         $input.addClass("is-invalid");
                         message = "Please fill the required multifield items";
-                    }else{
+                    } else {
                         $input.removeClass("is-invalid");
                     }
                 });
 
-                if(message){
+                if (message) {
                     $(".cq-dialog-submit").attr("disabled", "disabled");
                 }
 
                 return message;
             }
 
-            function show($el, message){
+            function show($el, message) {
 
-               /* jshint validthis: true */
-                if(this)
-                this.clear($el);
+                /* jshint validthis: true */
+
+                if (this)
+                    this.clear($el);
             }
 
-            function clear($el){
+            function clear($el) {
 
-				var value, min,max;
+                var value, min, max;
                 var $multifield = $el.closest(".coral-Multifield");
                 var val = $multifield.children('ol').children();
                 value = parseInt(val.length, 10);
                 min = $multifield.data('min');
                 max = $multifield.data('max');
-
-                console.log('shiva min in touch UI clear value:' + value);
-				if (value > max) {
-					 $(".cq-dialog-submit").attr("disabled", "disabled");
+                if (value > max) {
+                    $(".cq-dialog-submit").attr("disabled", "disabled");
                 } else if (value < min) {
-						 $(".cq-dialog-submit").attr("disabled", "disabled");
-                }else{
-					$(".cq-dialog-submit").removeAttr("disabled");
+                    $(".cq-dialog-submit").attr("disabled", "disabled");
+                } else {
+                    $(".cq-dialog-submit").removeAttr("disabled");
                     $multifield.nextAll(".coral-Form-fielderror").tooltip("hide").remove();
                 }
 
