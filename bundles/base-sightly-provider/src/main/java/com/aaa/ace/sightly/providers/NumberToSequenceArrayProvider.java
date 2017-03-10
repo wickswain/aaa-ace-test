87e1fba03a6549f68539ad1e88b571be97e40a24
Sridhar Yadav Mandra <sridhar.mandra@valuelabs.com>
2017-03-10 13:06:04 +0530

    AAA-481-commited chnages

---
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/NumberToSequenceArrayProvider.java
@@ -0,0 +1,38 @@
+package com.aaa.ace.sightly.providers;
+
+import java.util.ArrayList;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+/**
+ * This sightly provider is used to get the child resource properties of current
+ * resource.
+ *
+ */
+public class NumberToSequenceArrayProvider extends WCMUsePojo {
+
+	/**
+	 * List variable.
+	 */
+	private ArrayList<Integer> list;
+
+	@Override
+	public void activate() throws Exception {
+		String number = get("number", String.class);
+		int length = Integer.parseInt(number);
+		list = new ArrayList<Integer>();
+
+		for (int i = 0; i < length; i++) {
+			list.add(i);
+		}
+	}
+
+	/**
+	 * gets the list containing numbers
+	 *
+	 * @return list
+	 */
+	public ArrayList<Integer> getList() {
+		return list;
+	}
+}
