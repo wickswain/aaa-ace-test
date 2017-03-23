983b224b8a165ed174c1bc6687ca1cd4186acb68
Bharath Kambam <bkambam@axis41.com>
2017-03-23 19:17:36 +0530

    [AAA-490]: Publish page workflow for production and review servers.

--- bundles/base-service/src/main/java/com/aaa/ace/workflow/process/PublishPageProcess.java
+++ bundles/base-service/src/main/java/com/aaa/ace/workflow/process/PublishPageProcess.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
@@ -25,7 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aaa.ace.common.Constants;
-import com.aaa.ace.services.AEMUtilityService;
 import com.adobe.granite.workflow.WorkflowException;
 import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
@@ -35,8 +33,11 @@ import com.adobe.granite.workflow.metadata.MetaDataMap;
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.api.DamConstants;
 import com.day.cq.dam.commons.util.AssetReferenceSearch;
+import com.day.cq.replication.Agent;
+import com.day.cq.replication.AgentFilter;
 import com.day.cq.replication.ReplicationActionType;
 import com.day.cq.replication.ReplicationException;
+import com.day.cq.replication.ReplicationOptions;
 import com.day.cq.replication.Replicator;
 import com.day.cq.search.PredicateGroup;
 import com.day.cq.search.Query;
@@ -46,43 +47,60 @@ import com.day.cq.search.result.SearchResult;
 import com.day.cq.wcm.api.Page;
 
 /**
- * Publish the page content and it's asset, campaigns reference
+ * Publish the page content, assets, and campaign references to a specific
+ * replication agent (review) on specific server.
  *
  * @author bharath.kambam
  *
  */
 @Component(metatype = false, immediate = false)
 @Properties({ @Property(name = "service.label", value = "Publish Page Workflow Process"),
-		@Property(name = "service.description", value = "Publish Page Workflow Process implementation."),
-		@Property(label = "Workflow Label", name = "process.label", value = "Publish Page Workflow Process", description = "Publish Page Workflow Process description") })
+		@Property(name = "service.description", value = "This workflow process replicate the content, asset and campaign references to specific replication agent configured on workflow arguments."),
+		@Property(label = "Workflow Label", name = "process.label", value = "Publish Page Workflow Process", description = "This workflow process replicate the content, asset and campaign references to specific replication agent configured on workflow arguments.") })
 @Service
 public class PublishPageProcess implements WorkflowProcess {
 
-	private static final Object CAMPAIGNS_PATH_PROPERTY = "path";
+	/**
+	 * Campaigns path workflow argument property.
+	 */
+	private static final String CAMPAIGNS_PATH_PROPERTY = "path";
 
-	/** Default log. **/
-	protected final Logger log = LoggerFactory.getLogger(this.getClass());
+	/**
+	 * Replication agent ID workflow argument property.
+	 */
+	private static final String REPLICATION_AGENT_ID_PROPERTY = "agentId";
 
+	/**
+	 * Logger variable.
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(PublishPageProcess.class);
+
+	/**
+	 * Resource resolver variable.
+	 */
 	private ResourceResolver resolver = null;
 
+	/**
+	 * Campaigns Path variable.
+	 */
 	private String campaignsPath;
 
 	@Reference
-	Replicator replicator;
+	private Replicator replicator;
 
 	@Reference
-	QueryBuilder builder;
+	private QueryBuilder builder;
 
 	@Reference
-	AEMUtilityService utilityService;
-
-	@Reference
-	ResourceResolverFactory resourceResolverFactory;
+	private ResourceResolverFactory resourceResolverFactory;
 
 	@Override
 	public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap args) throws WorkflowException {
+		logger.info("PublishPageReviewProcess execute method starts.");
+
 		String payloadPath = null;
 		Session adminSession = null;
+		String agentID = null;
 
 		try {
 			Map<String, Object> paramMap = new HashMap<String, Object>();
@@ -95,7 +113,7 @@ public class PublishPageProcess implements WorkflowProcess {
 			// Fetch the arguments from workflow meta data.
 			String processArgs = args.get(Constants.PROCESS_ARGS, String.class);
 			String[] argumnets = processArgs.split(Constants.COMMA);
-			log.info("Workflow process step arguments: " + processArgs);
+			logger.debug("Workflow process step arguments: " + processArgs);
 
 			// Split and separate the arguments received from workflow meta
 			// data.
@@ -104,49 +122,74 @@ public class PublishPageProcess implements WorkflowProcess {
 
 				if (argumentNameValue[0].equals(CAMPAIGNS_PATH_PROPERTY)) {
 					campaignsPath = argumentNameValue[1];
+				} else if (argumentNameValue[0].equals(REPLICATION_AGENT_ID_PROPERTY)) {
+					agentID = argumentNameValue[1];
 				}
 			}
+			logger.debug("Campigns path: " + campaignsPath);
 
 			// Get the payload path from workflow data.
 			WorkflowData workflowData = workItem.getWorkflowData();
+
 			if (workflowData.getPayloadType().equals(Constants.JCR_PATH)) {
 				List<String> replicatePaths = new ArrayList<String>();
 				payloadPath = workflowData.getPayload().toString();
-				log.info("Workflow payload path: " + payloadPath);
+				logger.debug("Workflow payload path: " + payloadPath);
+
 				replicatePaths.add(payloadPath);
 
+				logger.debug("Replicate paths count Before: ", replicatePaths.size());
+
 				// replicate the reference assets in page
-				/*Map<String, Asset> assetReferences = getAssetReferencesInPage(payloadPath, adminSession);
-				log.info("Asset references for Page {} are {}.", payloadPath, assetReferences.entrySet().toString());
-				for (Entry<String, Asset> assetReference : assetReferences.entrySet()) {
-					replicatePaths.add(assetReference.getValue().getPath());
-				}*/
+				/*
+				 * Map<String, Asset> assetReferences =
+				 * getAssetReferencesInPage(payloadPath, adminSession);
+				 * log.info("Asset references for Page {} are {}.", payloadPath,
+				 * assetReferences.entrySet().toString()); for (Entry<String,
+				 * Asset> assetReference : assetReferences.entrySet()) {
+				 * replicatePaths.add(assetReference.getValue().getPath()); }
+				 */
 
 				// replicate the reference campaigns in page
 				List<Hit> campaignReferences = getCampaignReferencesInPage(payloadPath, adminSession);
-				log.info("Campaigns references for Page {} are {}.", payloadPath, campaignReferences.toString());
-				for (Hit hit : campaignReferences) {
-					replicatePaths.add(hit.getPath());
+				logger.debug("Campaigns references for Page {} are {}.", payloadPath, campaignReferences.toString());
+
+				if (campaignReferences != null) {
+					for (Hit hit : campaignReferences) {
+						replicatePaths.add(hit.getPath());
+					}
 				}
 
-				for (String replicatePath : replicatePaths) {
-					replicator.replicate(adminSession, ReplicationActionType.ACTIVATE, replicatePath);
-				}
-				log.info("Page {} replicated successfull.", payloadPath);
+				logger.debug("Replicate paths count: ", replicatePaths.size());
 
+				if (agentID != null) {
+					ReplicationOptions replicationOpts = getReplicationOptions(agentID);
+					logger.debug("Replication Options in WF: " + replicationOpts.toString());
+
+					for (String replicatePath : replicatePaths) {
+						replicator.replicate(adminSession, ReplicationActionType.ACTIVATE, replicatePath,
+								replicationOpts);
+					}
+				} else {
+					for (String replicatePath : replicatePaths) {
+						replicator.replicate(adminSession, ReplicationActionType.ACTIVATE, replicatePath);
+					}
+				}
+
+				logger.debug("Page {} replicated successfully.", payloadPath);
 			}
 
 		} catch (LoginException e) {
-			log.error("Error occured while fetching the resource resolver from workflow session.");
+			logger.error("Error occured while fetching the resource resolver from workflow session.");
 			e.printStackTrace();
 		} catch (ReplicationException e) {
-			log.error("Error occured while replicating the resource from workflow.");
+			logger.error("Error occured while replicating the resource from workflow.");
 			e.printStackTrace();
 		} catch (RepositoryException e) {
-			log.error("Error occured while replicating the campaigns resource from workflow.");
+			logger.error("Error occured while replicating the campaigns resource from workflow.");
 			e.printStackTrace();
 		} catch (Exception e) {
-			log.error("Error occured while replicating resource from workflow.", e.getMessage());
+			logger.error("Error occured while replicating resource from workflow.", e.getMessage());
 			e.printStackTrace();
 		} finally {
 			if (adminSession != null) {
@@ -154,20 +197,49 @@ public class PublishPageProcess implements WorkflowProcess {
 			}
 		}
 
+		logger.info("PublishPageReviewProcess execute method ends.");
+	}
+
+	/**
+	 * Gets the replication options object.
+	 *
+	 * @return
+	 */
+	private ReplicationOptions getReplicationOptions(final String agentID) {
+		logger.info("PublishPageReviewProcess getReplicationOptions method starts.");
+		logger.debug("Replication Agent in WF: " + agentID);
+		
+		ReplicationOptions replicationOpts = new ReplicationOptions();
+
+		replicationOpts.setFilter(new AgentFilter() {
+			public boolean isIncluded(final Agent agent) {
+				logger.debug("Replication Agent in AEM: " + agent.getId());
+				return agentID.equals(agent.getId());
+			}
+		});
+
+		replicationOpts.setSuppressVersions(true);
+		replicationOpts.setSuppressStatusUpdate(true);
+
+		logger.info("PublishPageReviewProcess getReplicationOptions method ends.");
+		return replicationOpts;
 	}
 
 	public List<Hit> getCampaignReferencesInPage(final String pagePath, final Session adminSession) {
-		log.info("Campaign references to be retrieved for page {} ", pagePath);
+		logger.info("PublishPageReviewProcess getCampaignReferencesInPage method starts.");
+
 		List<Hit> campaignResults = null;
 		Resource resource = resolver.getResource(pagePath).adaptTo(Page.class).getContentResource();
 
+		logger.debug("Campaign references to be retrieved for page {} ", pagePath);
 		if (resource.hasChildren()) {
 			Iterator<Resource> childs = resource.listChildren();
 			while (childs.hasNext()) {
 				Resource childResource = (Resource) childs.next();
 
-				log.info("Campaign references to be retrieved for resource {} ", childResource.getPath());
 				List<Hit> results = getTargetedComponentResults(childResource.getPath(), adminSession);
+				logger.debug("Campaign references retrieved for page {} on resource {} count is: {}", pagePath,
+						childResource.getPath(), results.size());
 
 				if (campaignResults == null) {
 					campaignResults = results;
@@ -176,13 +248,16 @@ public class PublishPageProcess implements WorkflowProcess {
 				}
 			}
 		}
+		logger.debug("Campaign references retrieved for page {} total count is: {}", pagePath, campaignResults.size());
 
+		logger.info("PublishPageReviewProcess getCampaignReferencesInPage method ends.");
 		return campaignResults;
 	}
 
 	private List<Hit> getTargetedComponentResults(final String pagePath, final Session adminSession) {
-		List<Hit> campaignResults = new ArrayList<Hit>();
+		logger.info("PublishPageReviewProcess getTargetedComponentResults method starts.");
 
+		List<Hit> campaignResults = new ArrayList<Hit>();
 		Map<String, String> map = new HashMap<String, String>();
 
 		map.put("path", pagePath);
@@ -191,19 +266,18 @@ public class PublishPageProcess implements WorkflowProcess {
 		map.put("property", "location");
 		map.put("property.operation", "exists");
 
-		log.info("Query builder map properties: " + map.toString());
+		logger.debug("Query builder map properties: " + map.toString());
 		// Create a Query instance
 		Query query = builder.createQuery(PredicateGroup.create(map), adminSession);
 
 		// Get the query results
 		SearchResult result = query.getResult();
-		log.info("Query builder results Hits size: " + result.getHits().size());
+		logger.debug("Query builder results Hits size: " + result.getHits().size());
 
 		if (result.getHits().size() > 0) {
 			Iterator<Resource> resultResources = result.getResources();
 			while (resultResources.hasNext()) {
 				Resource resource = (Resource) resultResources.next();
-				log.info("Result resource: " + resource.getPath());
 				String location = resource.getValueMap().get("location", "");
 
 				if (StringUtils.isNotBlank(location)) {
@@ -215,12 +289,14 @@ public class PublishPageProcess implements WorkflowProcess {
 			}
 		}
 
+		logger.info("PublishPageReviewProcess getTargetedComponentResults method ends.");
 		return campaignResults;
 	}
 
 	private List<Hit> getCampaignResults(final String location, final Session adminSession) {
-		List<Hit> results = null;
+		logger.info("PublishPageReviewProcess getCampaignResults method starts.");
 
+		List<Hit> results = null;
 		Map<String, String> map = new HashMap<String, String>();
 
 		if (StringUtils.isBlank(campaignsPath)) {
@@ -233,33 +309,37 @@ public class PublishPageProcess implements WorkflowProcess {
 		map.put("property.value", location);
 		map.put("property.operation", "like");
 
-		log.info("Query builder map properties: " + map.toString());
+		logger.debug("Query builder map properties: " + map.toString());
 		// Create a Query instance
 		Query query = builder.createQuery(PredicateGroup.create(map), adminSession);
 
 		// Get the query results
 		SearchResult result = query.getResult();
-		log.info("Query builder results Hits size: " + result.getHits().size());
+		logger.debug("Query builder results Hits size: " + result.getHits().size());
 
 		if (result.getHits().size() > 0) {
 			results = result.getHits();
 		}
 
+		logger.info("PublishPageReviewProcess getCampaignResults method ends.");
 		return results;
 	}
 
 	public Map<String, Asset> getAssetReferencesInPage(final String pagePath, final Session adminSession)
 			throws RepositoryException {
+		logger.info("PublishPageReviewProcess getAssetReferencesInPage method starts.");
+
 		Resource resource = resolver.getResource(pagePath).adaptTo(Page.class).getContentResource();
-		log.info("Assets references to be retrieved for page {} ", resource.getPath());
 		AssetReferenceSearch referenceSearch = new AssetReferenceSearch(resource.adaptTo(Node.class),
 				DamConstants.MOUNTPOINT_ASSETS, resolver);
+		logger.debug("Assets references to be retrieved for page {} ", resource.getPath());
 
 		if (referenceSearch.search().size() > 0) {
-			log.info(" {} assets references on page {} ", referenceSearch.search().size(), pagePath);
+			logger.debug("{} assets references on page {} ", referenceSearch.search().size(), pagePath);
 			return referenceSearch.search();
 		}
 
+		logger.info("PublishPageReviewProcess getAssetReferencesInPage method ends.");
 		return null;
 	}
 
