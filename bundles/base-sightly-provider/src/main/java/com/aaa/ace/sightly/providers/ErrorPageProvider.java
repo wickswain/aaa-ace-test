b7d876c5196255e4f14808632047af5de778eec6
hardikdesai <hdesai@axis41.com>
2017-04-03 19:56:13 +0530

    [AAA-532]: Created Error Page template.

---
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/ErrorPageProvider.java
@@ -0,0 +1,32 @@
+package com.aaa.ace.sightly.providers;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class ErrorPageProvider extends WCMUsePojo{
+	
+	private String tempPath;
+	public boolean errorPageFlag= false;
+
+	@Override
+	public void activate() throws Exception {
+		
+		tempPath = getCurrentPage().getProperties().get("cq:template","");
+		System.out.println("Template path  : "+tempPath);
+		errorPageFlag = isErrorPage();
+	
+	}
+	
+	public boolean isErrorPage(){
+		if(tempPath.equals("/apps/aaa-core/templates/errorpage")){
+			
+			errorPageFlag=true;
+		}
+		return errorPageFlag;
+		
+	}
+
+	public String getTempPath() {
+		return tempPath;
+	}
+	
+}
