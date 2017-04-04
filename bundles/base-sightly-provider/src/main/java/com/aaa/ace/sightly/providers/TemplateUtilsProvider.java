5722388f3188bf54d36880a5f7c35d34b0f0ae5b
hardikdesai <hdesai@axis41.com>
2017-04-04 16:36:22 +0530

    [AAA-532]: Updated error page template

--- bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/TemplateUtilsProvider.java
+++ bundles/base-sightly-provider/src/main/java/com/aaa/ace/sightly/providers/TemplateUtilsProvider.java
@@ -2,36 +2,31 @@ package com.aaa.ace.sightly.providers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.adobe.cq.sightly.WCMUsePojo;
 
 /**
-* This sightly provider class is used to fetch the template path from the current page.
-* 
-*/
+ * This sightly provider class is used to fetch the template path from the
+ * current page.
+ * 
+ */
 public class TemplateUtilsProvider extends WCMUsePojo {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(TemplateUtilsProvider.class);
 
 	/**
-	* Error page template path variable.
-	*/
+	 * Error page template path variable.
+	 */
 	private static String errorPageTemplatePath = "/apps/aaa-core/templates/errorpage";
 	private String currentPageTemplatePath;
 	private boolean errorPageFlag = false;
-	
+
 	@Override
 	public void activate() throws Exception {
 		currentPageTemplatePath = getCurrentPage().getProperties().get("cq:template", "");
 		log.debug("Current page template path : " + currentPageTemplatePath);
-		errorPageFlag = isErrorPage();
-	}
-
-	public boolean isErrorPage() {
-		if (errorPageTemplatePath.equals(currentPageTemplatePath)) {
-			errorPageFlag = true;
+		if (currentPageTemplatePath != null) {
+			errorPageFlag = errorPageTemplatePath.equals(currentPageTemplatePath);
 		}
-		return errorPageFlag;
 	}
 
 	/**
@@ -42,9 +37,14 @@ public class TemplateUtilsProvider extends WCMUsePojo {
 	public String getCurrentPageTemplatePath() {
 		return currentPageTemplatePath;
 	}
-	
+
+	/**
+	 * gets the errorPageFlag
+	 *
+	 * @return errorPageFlag
+	 */
 	public boolean isErrorPageFlag() {
 		return errorPageFlag;
 	}
-	
+
 }
