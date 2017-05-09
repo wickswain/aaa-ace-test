1611ba6047f86aa6798caf7f79d3043fbd3966c6
Vinod Dangudubiyyapu <vdangudubiyyapu@axis41.com>
2017-05-09 15:04:11 +0530

    Code beautification

--- html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
+++ html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
@@ -2,29 +2,27 @@
  * Jump to Anchor with “noscript and target compere” approach.
  */
 (function() {
-	$('body').append('<input id="ajaxStopCalls" type="hidden" >');
-		var hashtag = window.location.hash,
+    $('body').append('<input id="ajaxStopCalls" type="hidden" >');
+    var hashtag = window.location.hash,
         navbarHeight = $('.navbar-fixed-top').height(),
         stickyNavbarHeight = $('.sticky-nav').height(),
         swingTime = 0;
-		if(hashtag){
-				var targetCount = $(document).find('.target').length * 2,
-				noscriptCount = $(document).find("noscript").length,
-				diffCount = noscriptCount - targetCount,
-				newInterval = null,
-				varCount = 0;
-			
-				var checkInterval = function(){
-					var hiddenInputVal = $("body").find("#ajaxStopCalls");
-					if(noscriptCount == diffCount && hiddenInputVal.length != 'undefined'){
-						clearInterval(newInterval);
-						jumpLinkTarget(hashtag, navbarHeight, stickyNavbarHeight, swingTime);
-						$("#ajaxStopCalls").remove();
-					}
-					else {
-						varCount++;
-					}
-			};
-			newInterval = setInterval(checkInterval, 100);			
-		}	
+    if (hashtag) {
+        var targetCount = $(document).find('.target').length * 2,
+            noscriptCount = $(document).find("noscript").length,
+            diffCount = noscriptCount - targetCount,
+            newInterval = null,
+            varCount = 0;
+        var checkInterval = function() {
+            var hiddenInputVal = $("body").find("#ajaxStopCalls");
+            if (noscriptCount == diffCount && hiddenInputVal.length != 'undefined') {
+                clearInterval(newInterval);
+                jumpLinkTarget(hashtag, navbarHeight, stickyNavbarHeight, swingTime);
+                $("#ajaxStopCalls").remove();
+            } else {
+                varCount++;
+            }
+        };
+        newInterval = setInterval(checkInterval, 100);
+    }
 })();
