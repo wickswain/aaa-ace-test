24d58a903047102d420b9edb33bc6fad1c29fb91
Vinod Dangudubiyyapu <vdangudubiyyapu@axis41.com>
2017-05-09 15:02:50 +0530

    [AAA-541-FED]:Control default jumplinks function by handling hashtag length.

--- html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
+++ html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
@@ -7,23 +7,24 @@
         navbarHeight = $('.navbar-fixed-top').height(),
         stickyNavbarHeight = $('.sticky-nav').height(),
         swingTime = 0;
-
-		var targetCount = $(document).find('.target').length * 2,
-    	noscriptCount = $(document).find("noscript").length,
-    	diffCount = noscriptCount - targetCount,
-    	newInterval = null,
-    	varCount = 0;
-    
-		var checkInterval = function(){
-            var hiddenInputVal = $("body").find("#ajaxStopCalls");
-            if(noscriptCount == diffCount && hiddenInputVal.length != 'undefined'){
-                clearInterval(newInterval);
-                jumpLinkTarget(hashtag, navbarHeight, stickyNavbarHeight, swingTime);
-                $("#ajaxStopCalls").remove();
-            }
-            else {
-                varCount++;
-            }
-    };
-    newInterval = setInterval(checkInterval, 100);
+		if(hashtag){
+				var targetCount = $(document).find('.target').length * 2,
+				noscriptCount = $(document).find("noscript").length,
+				diffCount = noscriptCount - targetCount,
+				newInterval = null,
+				varCount = 0;
+			
+				var checkInterval = function(){
+					var hiddenInputVal = $("body").find("#ajaxStopCalls");
+					if(noscriptCount == diffCount && hiddenInputVal.length != 'undefined'){
+						clearInterval(newInterval);
+						jumpLinkTarget(hashtag, navbarHeight, stickyNavbarHeight, swingTime);
+						$("#ajaxStopCalls").remove();
+					}
+					else {
+						varCount++;
+					}
+			};
+			newInterval = setInterval(checkInterval, 100);			
+		}	
 })();
