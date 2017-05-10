f39431d64dd2cc5842f5d8553ccb175fdeb94b1f
Bharath Kambam <bkambam@axis41.com>
2017-05-10 12:46:31 +0530

    [AAA]: No script count issue fix provided.

--- html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
+++ html-prototype/clientlibs/aaa-core/base/js/jumpToAnchorScript.js
@@ -1,28 +1,42 @@
 /**
- * Jump to Anchor with “noscript and target compere” approach.
+ * Jump to Anchor with “noscript and target compare” approach.
  */
 (function() {
-    $('body').append('<input id="ajaxStopCalls" type="hidden" >');
     var hashtag = window.location.hash,
         navbarHeight = $('.navbar-fixed-top').height(),
         stickyNavbarHeight = $('.sticky-nav').height(),
         swingTime = 0;
+    
+    //$('body').append('<input id="ajaxStopCalls" type="hidden" >');
+    
     if (hashtag) {
         var targetCount = $(document).find('.target').length * 2,
             noscriptCount = $(document).find("noscript").length,
             diffCount = noscriptCount - targetCount,
             newInterval = null,
             varCount = 0;
+        
+        $(document).ajaxComplete(function () {
+            var _nsCount = $(document).find("noscript").length;
+         
+            console.log("No Script count in ajax complete: " + _nsCount);
+        });
+        
         var checkInterval = function() {
-            var hiddenInputVal = $("body").find("#ajaxStopCalls");
-            if (noscriptCount == diffCount && hiddenInputVal.length != 'undefined') {
+            var hiddenInputVal = $("body").find("#ajaxStopCalls"),
+            	noscriptCount = $(document).find("noscript").length;
+            console.log("No Script count in Interval: " + noscriptCount);
+            
+            if (noscriptCount <= diffCount) {
                 clearInterval(newInterval);
                 jumpLinkTarget(hashtag, navbarHeight, stickyNavbarHeight, swingTime);
-                $("#ajaxStopCalls").remove();
+                //$("#ajaxStopCalls").remove();
             } else {
                 varCount++;
             }
         };
+        
         newInterval = setInterval(checkInterval, 100);
     }
+    
 })();
